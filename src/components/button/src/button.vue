540566b5b4dcb2f554d60455c8f3006b1981d4aa
wuyin <wuyin@do1.com.cn>
2020-03-31 18:33:05 +0800

    button init

---
+++ src/components/button/src/button.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>我是全局应用的按钮</div>
+</template>
+
+<script>
+const COMPONENT_NAME = 'wy-button'
+export default {
+  name: COMPONENT_NAME,
+  components: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  methods: {}
+}
+
+</script>
+<style lang='scss' scoped>
+</style>
---
+++ src/moudule/menu.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>111</div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  methods: {}
+}
+
+</script>
+<style lang='scss' scoped>
+</style>
